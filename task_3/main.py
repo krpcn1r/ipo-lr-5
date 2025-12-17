83c62d70af4f7e88d618bfc1791db3438f9e8492
krpcn1r <zaystep09@gmail.com>
2025-12-17 20:30:18 +0300

    Update main.py

--- task_3/main.py
+++ task_3/main.py
@@ -1,3 +1,4 @@
+#Зайцев Степан
 with open("C:/Users/ZaytsSte_89/Desktop/ИПО/lr5/text.txt", "r", encoding = "UTF-8") as file: # Открываю файл для чтения
     text = file.readline() # Заношу в переменную 1-ю строку
     num = text.split() # Убираю пробелы
